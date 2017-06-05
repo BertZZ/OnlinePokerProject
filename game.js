0f3b485117d1804105e3de02412d1b5c538fc1ef
Bertie Wooles <bertie.wooles@hotmail.co.uk>
2017-06-05 18:09:40 +0100

    Deck Deals 2 cards to each player depending on number of players in the game

--- game.js
+++ game.js
@@ -1,9 +1,22 @@
-function Game() {
-  this.players = 0
+function Game(players) {
+  this.players = players
+  this.hands = []
+  this.cards = [1,2,3,4,5,6,7,8,9,10]
 }
 
-Game.prototype.setPlayers = function(number){
-  this.players = number
+Game.prototype.deal = function(){
+  for (i = 0; i < this.players; i++) {
+    var hand = []
+    var card = this.cards.shift()
+    console.log(card)
+    hand.push(card);
+    console.log(hand)
+    this.hands.push(hand)
+  }
+  for (i = 0; i < this.players; i++) {
+    var card = this.cards.shift()
+    this.hands[i].push(card)
+  }
 }
 
 module.exports = Game;
--- test/gameSpec.js
+++ test/gameSpec.js
@@ -4,17 +4,38 @@ var expect = require('chai').expect
 
 const Game = require('../game.js')
 
-describe('5', function() {
-  it('should be 5', function(done) {
-    (5).should.be.exactly(5).and.be.a.Number();
-    done();
-  })
-})
-
 describe("Game", function() {
   it('should exist', function(done) {
-    var game = new Game
+    var game = new Game()
     expect(game).to.not.be.undefined
+    done()
+  })
+
+  it('should set number of players', function(done) {
+    var game = new Game(3)
+    expect(game.players).to.equal(3)
+    done()
+  })
+
+  it('should start with an empty hands array', function(done) {
+    var game = new Game(3)
+    expect(game.hands).to.be.an('array').that.is.empty;
+    done()
+  })
+
+  it('should deal 2 cards from the top of the deck to each player', function(done){
+    var game = new Game(3)
+    game.deal();
+    expect(game.hands[0]).to.be.an('array').that.is.not.empty;
+    expect(game.hands[0]).to.have.lengthOf(2);
+    expect(game.hands[0]).to.deep.equal([1,4])
+    expect(game.hands[1]).to.be.an('array').that.is.not.empty;
+    expect(game.hands[1]).to.have.lengthOf(2);
+    expect(game.hands[1]).to.deep.equal([2,5])
+    expect(game.hands[2]).to.be.an('array').that.is.not.empty;
+    expect(game.hands[2]).to.have.lengthOf(2);
+    expect(game.hands[2]).to.deep.equal([3,6])
+    expect(game.hands[3]).to.be.undefined
     done()
   })
 })
