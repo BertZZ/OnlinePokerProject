cfd7a9c552452f79b937c17ad1fb6005d773f333
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2017-06-05 15:56:18 +0100

    adds checkdeck and shuffleDeck

--- test/cardDeckSpec.js
+++ test/cardDeckSpec.js
@@ -9,10 +9,27 @@ beforeEach(function() {
   cardDeck = new CardDeck();
 });
 
-describe('App', function() {
-  describe('#cardDeck()', function() {
-    it('should contain 52 cards', function() {
-      expect(cardDeck.deck.length).to.equal(52);
+describe('CardDeck', function() {
+  it('should have a deck containing 52 cards', function() {
+    expect(cardDeck.deck.length).to.equal(52);
+  });
+
+  describe('#shuffleDeck', function() {
+    it('should shuffle the deck array', function() {
+      cardDeck.shuffleDeck();
+      expect(cardDeck.deck[0]).not.to.equal(1);
+    });
+
+    it('should contian 52 unique cards', function() {
+      cardDeck.shuffleDeck();
+      expect(cardDeck.checkDeck()).to.equal(true);
     });
   });
-})
+
+  describe('#deckCheck', function() {
+    it('should check that no duplicates exist in the deck', function() {
+      expect(cardDeck.checkDeck([1, 2])).to.equal(true);
+      expect(cardDeck.checkDeck([1, 1])).to.equal(false);
+    });
+  });
+});
