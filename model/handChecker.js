e3ca99aec10343bb3dce718f502f7ed7c486ebd0
Peter Barcsak <peter.barcsak@gmail.com>
2017-06-06 14:25:38 +0100

    Some refactoring

--- model/handChecker.js
+++ model/handChecker.js
@@ -1,33 +1,61 @@
-function HandChecker() {
+function HandChecker(hand) {
+  this.sortedHand = sortCards(hand)
+  this.sortedCardValues = valueSuitSplitter(this.sortedHand)[0]
+  this.sortedCardSuits = valueSuitSplitter(this.sortedHand)[1]
+
+  function sortCards(handToSort) {
+    var suits = ['s', 'h', 'd', 'c']
+    var cards = [ 'A', 'K', 'Q', 'J', 'T', 9, 8, 7, 6, 5, 4, 3, 2 ];
+    var cardsOrder = []
+    cards.forEach(function(card){
+      suits.forEach(function(suit){
+        cardsOrder.push(card + suit);
+      });
+    });
+    return handToSort.sort(function(a, b) {
+      return cardsOrder.indexOf(a) - cardsOrder.indexOf(b);
+    });
+  };
+
+  function valueSuitSplitter(cards) {
+      splitedCards = [[],[]];
+      for ( var i = 0; i < cards.length; i++ ) {
+        card = cards[i].split('')
+        splitedCards[0].push(card[0])
+        splitedCards[1].push(card[1])
+      }
+      return splitedCards
+    }
 };
 
-HandChecker.prototype.poker = function(cards) {
-  if (this.modeAndFrequency(cards)[1].includes(4)) return true
+HandChecker.prototype.poker = function() {
+  if (this.modeAndFrequency(this.sortedCardValues)[1].includes(4)) return true
 }
 
-HandChecker.prototype.flush = function(cards) {
-  if (this.modeAndFrequency(Object.values(this.valueSuitSplitter(cards)))[1][0] >= 5) return true
+HandChecker.prototype.flush = function() {
+  if (this.modeAndFrequency(this.sortedCardSuits)[1] >= 5) return true
 }
 
-HandChecker.prototype.straight = function(cards) {
+HandChecker.prototype.straight = function() {
   var straightCards = "AKQJT98765432A5432"
-  if (straightCards.includes(this.sortCards(cards).join(''))) return true
+  console.log(this.sortedCardValues.join(''))
+  if (straightCards.includes(this.sortedCardValues.join(''))) return true
 }
 
-HandChecker.prototype.fullHouse = function(cards) {
-  if (this.modeAndFrequency(cards)[1].includes(3) && this.modeAndFrequency(cards)[1].includes(2)) return true
+HandChecker.prototype.fullHouse = function() {
+  if (this.modeAndFrequency(this.sortedCardValues)[1].includes(3) && this.modeAndFrequency(this.sortedCardValues)[1].includes(2)) return true
+}
+//
+HandChecker.prototype.threeOfAKind = function() {
+  if (this.modeAndFrequency(this.sortedCardValues)[1].includes(3)) return true
 }
 
-HandChecker.prototype.threeOfAKind = function(cards) {
-  if (this.modeAndFrequency(cards)[1].includes(3)) return true
-}
-
-HandChecker.prototype.twoPair = function(cards) {
-  if (this.modeAndFrequency(cards)[1][0] === 2 && this.modeAndFrequency(cards)[1][1] === 2) return true
+HandChecker.prototype.twoPair = function() {
+  if (this.modeAndFrequency(this.sortedCardValues)[1][0] === 2 && this.modeAndFrequency(this.sortedCardValues)[1][1] === 2) return true
 }
 
 HandChecker.prototype.pair = function(cards) {
-  if (this.modeAndFrequency(cards)[1].includes(2)) return true
+  if (this.modeAndFrequency(this.sortedCardValues)[1].includes(2)) return true
 }
 
 HandChecker.prototype.modeAndFrequency = function(arr) {
@@ -44,24 +72,5 @@ HandChecker.prototype.modeAndFrequency = function(arr) {
   }
   return [a, b];
 }
-
-HandChecker.prototype.valueSuitSplitter = function(cards) {
-  cardsHash = {};
-  for ( var i = 0; i < cards.length; i++ ) {
-      card = cards[i].split('')
-      cardsHash[card[0]] = card[1];
-  }
-  return cardsHash
-}
-
-HandChecker.prototype.sortCards = function(cards) {
-  var cardsOrder = [ 'A', 'K', 'Q', 'J', 'T', 9, 8, 7, 6, 5, 4, 3, 2 ];
-
-  return cards.sort(function(a, b) {
-    return cardsOrder.indexOf(a) - cardsOrder.indexOf(b);
-  });
-};
-
-Object.values = obj => Object.keys(obj).map(key => obj[key]);
 
 module.exports = HandChecker;
--- test/handCheckerSpec.js
+++ test/handCheckerSpec.js
@@ -8,43 +8,50 @@ describe('HandChecker', function() {
   var handChecker;
   var cards;
 
-  beforeEach(function() {
-    handChecker = new HandChecker();
-  });
+  // beforeEach(function(currentCards) {
+  //   handChecker = new HandChecker(cards);
+  // });
 
   it('returns true for a pair', function() {
-    cards = [3,3,5,6,7]
-    expect(handChecker.pair(cards)).to.equal(true);
+    cards = ['3s','3c','5h','6h','7h', 'Ts', ]
+    handChecker = new HandChecker(cards)
+    expect(handChecker.pair()).to.equal(true);
   });
 
   it('returns true for two pair', function() {
-    cards = [3,3,5,5,7]
-    expect(handChecker.twoPair(cards)).to.equal(true);
+    cards = ['3s','3h','5d','5c','7d']
+    handChecker = new HandChecker(cards)
+    expect(handChecker.twoPair()).to.equal(true);
   });
 
   it('returns true for three of a kind', function() {
-    cards = [6,6,6,5,7]
+    cards = ['6s','6c','6h','5h','7d']
+    handChecker = new HandChecker(cards)
     expect(handChecker.threeOfAKind(cards)).to.equal(true);
   });
 
   it('returns true for full house', function() {
-    cards = [6,6,5,5,5]
-    expect(handChecker.fullHouse(cards)).to.equal(true);
+    cards = ['6s','6c','5s','5c','5h']
+    handChecker = new HandChecker(cards)
+    expect(handChecker.fullHouse()).to.equal(true);
   });
 
-  it('returns true for full house', function() {
+  it('returns true for flush', function() {
     cards = ['As', '2s', 'Js', '6s', '7s']
-    expect(handChecker.flush(cards)).to.equal(true)
+    handChecker = new HandChecker(cards)
+    expect(handChecker.flush()).to.equal(true)
   })
 
   it('returns true for a straight', function() {
-    cards = [ 'A', 2, 3, 4, 5 ]
+    cards = [ 'Ts', 'Js', '9c', '8h', 'Qd' ]
+    handChecker = new HandChecker(cards)
     expect(handChecker.straight(cards)).to.equal(true)
   });
 
   it('returns true for a poker', function() {
-    cards = [ 'A', 'A', 'A', 'A', 'K']
-    expect(handChecker.poker(cards)).to.equal(true)
+    cards = [ 'As', 'Ad', 'Ac', 'Ah', 'Ks']
+    handChecker = new HandChecker(cards)
+    expect(handChecker.poker()).to.equal(true)
   })
 
 });
