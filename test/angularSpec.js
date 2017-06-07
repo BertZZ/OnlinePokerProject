3f1e8a3f1b73a49974420aae1b1244824ac4cf26
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2017-06-07 16:31:37 +0100

    adds final testing structure

--- test/angularSpec.js
+++ test/angularSpec.js
@@ -9,19 +9,33 @@ Browser.localhost('example.com', 3000);
 // get the application server module
 // var app = require('../app.js');
 
-describe('home page', function() {
+describe('Home Page', function() {
 
  const browser = new Browser();
 
   before(function(done) {
     return browser.visit('/', done);
   });
+
    describe('loading page', function() {
-    it('should be successful', function() {
-     browser.assert.success();
+
+     it('should be successful', function() {
+       browser.assert.success();
+     });
+
+     it('should have a tile', function() {
+       browser.assert.text('h1', 'Poker');
+     });
    });
-   it('should have a tile', function() {
-      browser.assert.text('h1', 'Poker');
-    });
- });
+
+   describe('Deal Communal', function() {
+     it('a back is there before the button is pressed', function() {
+       browser.assert.elements("#back", 7);
+     });
+
+     it('a king is there after the button is pressed', function() {
+       browser.pressButton("DEAL COMMUNAL");
+       browser.assert.elements("#king_s", 1);
+     });
+   });
 });
