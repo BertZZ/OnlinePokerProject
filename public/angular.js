7e663eca79ad96245128ac45de8916031322106c
Unai Motriko <unaimotriko@gmail.com>
2017-06-07 12:52:19 +0100

    initiates mocha testing setup for angular

--- public/angular.js
+++ public/angular.js
@@ -5,13 +5,14 @@ angular.module('ViewController',[])
 
   function PlayerRender($scope){
     $scope.playerCards = ['back', 'back'];
-    console.log($scope.playerCards);
-    // $scope.communalCards;
+    $scope.communalCards = ['back','back','back','back','back'];
+    console.log($scope.communalCards);
 
     $scope.dealPlayer = function() {
       $scope.playerCards = ['ace_s', 'queen_h'];
     };
-    // $scope.dealCommunal = function(){
-    //   $scope.communalCards = ['king_s', 'two_c', 'king_h', 'jack_d', 'queen_h'];
-    // }
+
+    $scope.dealCommunal = function(){
+      $scope.communalCards = ['king_s', 'two_c', 'king_h', 'jack_d', 'queen_s'];
+    }
   }
---
+++ test/angularSpec.js
@@ -0,0 +1,5 @@
+require('angular')
+var assert = require('assert');
+var expect = require('chai').expect
+
+const PlayerRender = require('../public/angular.js')
