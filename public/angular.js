3e696771d17790c6e9250c9bd7f47dcc40411ff2
Unai Motriko <unaimotriko@gmail.com>
2017-06-08 13:43:06 +0100

    adds angular ui controller

--- public/angular.js
+++ public/angular.js
@@ -1,22 +1,45 @@
-angular.module('ViewController', ['$location', 'ngResource', 'ngRoute'])
-  .controller('PlayerRender',['$scope', PlayerRender]);
+var myApp = angular.module('ViewController', ['ui.router'])
 
-  function PlayerRender($scope){
-    $scope.playerCards = ['back', 'back'];
-    $scope.communalCards = ['back','back','back','back','back'];
-    $scope.screenURL = "";
+myApp.config(function($stateProvider) {
+  // var helloState = {
+  //   name: 'hello',
+  //   url: '/hello',
+  //   template: '</br><a ui-sref="about">Deal</a>'
+  // }
 
-    $scope.dealPlayer = function() {
-      $scope.playerCards = ['ace_s', 'queen_h'];
-    };
+  var aboutState = {
+    name: 'about',
+    url: '/start',
+    component: 'start'
 
-    $scope.dealCommunal = function(){
-      $scope.communalCards = ['king_s', 'two_c', 'king_h', 'jack_d', 'queen_s'];
-    }
 
-    $scope.changeURL = function() {
-      $scope.screenURL = 'dealURL';
-      $location.url('/show_cards.html');
-      console.log('working');
-    }
   }
+
+  // $stateProvider.state(helloState);
+  $stateProvider.state(aboutState);
+});
+
+
+
+
+  // .controller('PlayerRender',['$scope', PlayerRender]);
+  //
+  // function PlayerRender($scope){
+  //   $scope.playerCards = ['back', 'back'];
+  //   $scope.communalCards = ['back','back','back','back','back'];
+  //   $scope.screenURL = "";
+  //
+  //   $scope.dealPlayer = function() {
+  //     $scope.playerCards = ['ace_s', 'queen_h'];
+  //   };
+  //
+  //   $scope.dealCommunal = function(){
+  //     $scope.communalCards = ['king_s', 'two_c', 'king_h', 'jack_d', 'queen_s'];
+  //   }
+  //
+  //   $scope.changeURL = function() {
+  //     $scope.screenURL = 'dealURL';
+  //     // $location.url('/show_cards.html');
+  //     console.log('working');
+  //   }
+  // }
